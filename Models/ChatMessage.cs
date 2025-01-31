0860ce8dfcf3b8b0e75de3ca5fbeea7f1603cbf2
yunsong <laozhu_shangxin@hotmail.com>
2025-02-01 02:18:52 +0800

    Refactor ChatMessage to inherit from BindableBase and implement property change notifications for Content and IsUser

--- Models/ChatMessage.cs
+++ Models/ChatMessage.cs
@@ -1,9 +1,23 @@
+using Prism.Mvvm;
+
 namespace ollez.Models
 {
-    public class ChatMessage
+    public class ChatMessage : BindableBase
     {
-        public string Content { get; set; }
-        public bool IsUser { get; set; }
+        private string _content;
+        public string Content
+        {
+            get => _content;
+            set => SetProperty(ref _content, value);
+        }
+
+        private bool _isUser;
+        public bool IsUser
+        {
+            get => _isUser;
+            set => SetProperty(ref _isUser, value);
+        }
+
         public string Role => IsUser ? "user" : "assistant";
     }
-} 
+}
