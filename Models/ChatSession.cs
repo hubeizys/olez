3883449c5e30dc6415987f0180f095d8c4b3fd65
yunsong <laozhu_shangxin@hotmail.com>
2025-02-03 00:37:22 +0800

    Add LastMessageTime property to ChatSession model to track the time of the last message; ensure default behavior when no messages are present.

--- Models/ChatSession.cs
+++ Models/ChatSession.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace ollez.Models
 {
@@ -11,5 +12,7 @@ namespace ollez.Models
         public string Title { get; set; } = "新会话";
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public ObservableCollection<ChatMessage> Messages { get; set; } = new ObservableCollection<ChatMessage>();
+
+        public DateTime LastMessageTime => Messages.Any() ? Messages.Last().CreatedAt : CreatedAt;
     }
 }
